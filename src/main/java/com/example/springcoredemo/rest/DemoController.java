aeae8b2c5703e71152243d3a01a876c7d32dee15
ifshaan02 <ifshaan02@gmail.com>
2025-08-29 21:12:16 +0530

    Add files via upload

---
+++ src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springcoredemo.common.Coach;
+
+@RestController
+public class DemoController {
+	
+	// define a private field for the dependency
+	
+	private Coach myCoach;
+	
+	// define constructor for dependency injection
+	
+	@Autowired
+	public DemoController(Coach theCoach) {
+		myCoach = theCoach;
+	}
+	
+	@GetMapping("/dailyworkout")
+	
+	public String getDailyWorkout() {
+		return myCoach.getDailyWorkout();
+	}
+
+	
+}
